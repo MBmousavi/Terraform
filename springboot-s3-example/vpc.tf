f31cea1865f6c344a9f05980db2ea9d72e88baed
Mohammad Mousavi <38520491+MBmousavi@users.noreply.github.com>
2021-12-02 18:39:55 +0330

    Update vpc.tf

--- springboot-s3-example/vpc.tf
+++ springboot-s3-example/vpc.tf
@@ -74,8 +74,7 @@ resource "aws_security_group" "ec2" {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    #security_groups = [aws_security_group.alb.id]
+    security_groups = [aws_security_group.alb.id]
   }
   tags          = { Name = "${var.route53_hosted_zone_name}-ec2-security-group" }
 }
