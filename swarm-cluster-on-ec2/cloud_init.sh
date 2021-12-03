31eafea6c46cef5d88103bb8554b3ed90c5fc067
Mohammad Mousavi <38520491+MBmousavi@users.noreply.github.com>
2021-12-03 16:58:57 +0330

    Create cloud_init.sh

---
+++ swarm-cluster-on-ec2/cloud_init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt install docker-ce -y
+systemctl start docker
+usermod -aG docker ubuntu
