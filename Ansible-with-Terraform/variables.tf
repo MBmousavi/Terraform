25faf422429e287a8833b13447d2c0c6c2b94fed
Mohammad Mousavi <38520491+MBmousavi@users.noreply.github.com>
2022-01-11 14:33:36 +0330

    Create variables.tf

---
+++ Ansible-with-Terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {}
+variable "instance_count" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "ssh_key_private" {}
+variable "inventory_file" {}
